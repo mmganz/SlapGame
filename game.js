f5d42dc491996b279738aea51005517392aa7789
Meredith Ganz <2meredithganz@gmail.com>
2016-09-23 13:54:54 -0600

    push

---
+++ game.js
@@ -0,0 +1,28 @@
+var health = 100; 
+var slap = 5; 
+var punch = 10;
+var kick = 20; 
+
+function onSlap(){
+  health -= slap
+  update()
+  return health 
+}
+function onPunch(){
+  health -= punch
+  update()
+  return health
+}
+
+function reset(){
+  health = 100
+  update()
+}
+
+function update(){
+  if (health <= 0){
+    health = 0 
+  }
+  var healthElem = document.getElementById('health')
+  healthElem.innerHTML = health
+}
