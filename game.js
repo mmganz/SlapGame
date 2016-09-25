471c9983aa024e8d98da8f3360bbd075837cf6f9
Meredith Ganz <2meredithganz@gmail.com>
2016-09-24 20:28:21 -0600

    f

--- game.js
+++ game.js
@@ -38,28 +38,28 @@ function update(){
 
 function changeImage(){ 
     var cImage = document.getElementById('image')
-    if(p1.health == 0){
-        cImage.src="flowers/bulb.png"
-    }
-    else if(p1.health > 0 && p1.health < 10){
-        cImage.src="flowers/bulb.png"
+    if(p1.health > 0 && p1.health < 10){
+        cImage.src="flowers/roots.png"
     }else if(p1.health >=10 && p1.health <20){
-        cImage.src="flowers/1.png"
+        cImage.src="flowers/bulb.png"
     }else if(p1.health >=20 && p1.health <30){
-        cImage.src="flowers/2.png"
+        cImage.src="flowers/1.png"
     }else if(p1.health >=30 && p1.health <40){
-        cImage.src="flowers/3.png"
+        cImage.src="flowers/2.png"
     }else if(p1.health >=40 && p1.health <50){
-        cImage.src="flowers/4.png"
+        cImage.src="flowers/3.png"
     }else if(p1.health >=50 && p1.health <60){
-        cImage.src="flowers/5.png"
+        cImage.src="flowers/4.png"
     }else if(p1.health >=60 && p1.health <70){
-        cImage.src="flowers/6.png"
+        cImage.src="flowers/5.png"
     }else if(p1.health >=70 && p1.health <80){
+        cImage.src="flowers/6.png"
+    }else if(p1.health>=80 && p1.health <90){
         cImage.src="flowers/7.png"
-    }else if(p1.health>=80 && p1.health <99){
+    }else if(p1.health>=90 && p1.health <99){
         cImage.src="flowers/8.png"
-    }else if(p1.health >99){
+    }
+    else(p1.health >99){
         cImage.src="flowers/large.gif"
     }
     }
